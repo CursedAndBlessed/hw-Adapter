152088eb1e865cd0b208fda6f9d9c2dd9c4b8652
CursedAndBlessed <8udzumyki@gmail.com>
2022-04-27 14:07:56 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Ints calc = new IntsCalculator();
+        System.out.println(calc.mult(40, 230));
+        System.out.println(calc.pow(5, 5));
+    }
+}
+
